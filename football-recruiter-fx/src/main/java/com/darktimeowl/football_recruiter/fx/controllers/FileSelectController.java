de2158052d25937c03e77b50b4f5d935a4f3d0d3
DarktimeOwl <darktimeowl@gmail.com>
2023-12-09 00:05:48 -0600

    Set up skeleton of file select screen

--- football-recruiter-fx/src/main/java/com/darktimeowl/football_recruiter/fx/controllers/FileSelectController.java
+++ football-recruiter-fx/src/main/java/com/darktimeowl/football_recruiter/fx/controllers/FileSelectController.java
@@ -1,5 +1,35 @@
 package com.darktimeowl.football_recruiter.fx.controllers;
 
-public class FileSelectController extends Controller {
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
 
+public class FileSelectController extends Controller {
+    @FXML private ListView<String> fileListView;
+    private ObservableList<String> files;
+
+    @FXML
+    private void initialize() {
+        files = FXCollections.observableArrayList();
+        fileListView.setItems(files);
+    }
+
+    @FXML
+    private void clickOpen() {
+        files.add("Open clicked");
+    }
+
+    @FXML
+    private void clickCancel() {
+        files.add("Cancel clicked");
+    }
+
+    @FXML
+    private void clickDelete() {
+        int selected = fileListView.getFocusModel().getFocusedIndex();
+        if (selected >= 0) {
+            fileListView.getItems().remove(selected);
+        }
+    }
 }
